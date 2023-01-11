4f72c26eb1470ecac3345740eec162016db33177
GAXALAR <ay.kosyrev@gmail.com>
2023-01-11 12:24:38 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int i =0;
+        int sum = 0;
+        while (i < 1000) {
+            if (i % 3 == 0 || i % 5 == 0){
+                sum = sum + i;
+            }
+                i++;
+
+        }
+
+        System.out.println(sum);
+    }
+}
